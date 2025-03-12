58da6dcc76bf28aa3158924b21de4cd21e8b35b1
VictorSantilli <VictorSantilli@users.noreply.github.com>
2025-03-12 13:52:04 -0300

    Atualizando algumas infos

---
+++ FRONT/js/home.js
@@ -0,0 +1,51 @@
+const limite = 5; // Produtos por página
+
+// Função para buscar produtos da API
+function buscarProdutos(pagina = 0, termoBusca = '') {
+    const skip = pagina * limite;
+    let url = `https://dummyjson.com/products?limit=${limite}&skip=${skip}`;
+
+    if (termoBusca !== '') {
+        url = `https://dummyjson.com/products/search?q=${termoBusca}&limit=${limite}&skip=${skip}`;
+    }
+
+    return fetch(url)
+        .then(res => res.json())
+        .then(data => data.products)
+        .catch(erro => {
+            console.error('Erro ao buscar produtos:', erro);
+            return [];
+        });
+}
+
+// Atualiza a tabela com os produtos
+function atualizarTabela(produtos) {
+    const tabelaCorpo = document.getElementById('tableListProducts-body');
+    tabelaCorpo.innerHTML = '';
+
+    if (produtos.length === 0) {
+        tabelaCorpo.innerHTML = '<tr><td colspan="5">Nenhum produto encontrado.</td></tr>';
+        return;
+    }
+
+    produtos.forEach(produto => {
+        const linha = document.createElement('tr');
+        linha.innerHTML = `
+            <td>${produto.id}</td>
+            <td>${produto.title}</td>
+            <td>${produto.category}</td>
+            <td>R$ ${produto.price.toFixed(2)}</td>
+            <td><img src="">${produto.stock}</td>
+        `;
+        tabelaCorpo.appendChild(linha);
+    });
+}
+
+
+// Chama a função para carregar os produtos automaticamente ao abrir a página
+document.addEventListener('DOMContentLoaded', () => {
+    buscarProdutos(0).then(produtos => {
+        atualizarTabela(produtos);  // Atualiza a tabela com os produtos
+        atualizarPagina(0);         // Exibe a página inicial
+    });
+});
